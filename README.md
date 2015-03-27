dad69bede0f2da73407a4de95e2a2a1600986ce4
Cameron Sun <cameron.csun@gmail.com>
2015-03-27 00:59:20 -0400

    generate seems to work

---
+++ README.md
@@ -0,0 +1,43 @@
+# pixel_pusher
+A toolkit and visualizer for the Optimization question @ Northeastern's Code Showdown 2015
+
+## Requirements
+A system with python installed
+
+## File Formats
+All pixel_pusher scripts either input or output files of the following types:
+
+### Palette
+Defines an ordering of pixels in the palette. The first line is N, the number of rows / columns the square palette has. The next N lines contain N space-separated values of each pixel. An asterisk is used for the hole in the palette.
+	
+	4
+	1 2 3 4
+	5 6 7 8	
+	9 10 11 *
+	13 14 15 12
+
+### Pushes
+Defines an ordered list of pixel-pushes. The first line is M, the number of pushes contained in the file. The next M lines contain R and C, two space separated integers denoting the row and column of the pixel to be moved
+	
+	1
+	3 3
+
+Note that this push list solves the palette above.
+
+## Scripts
+There are three scripts included in this repository to help you get started.
+
+### check
+`./check PALETTE_PATH PUSHES_PATH`
+
+Takes in a path to a palette file and a pushes file and outputs wheter or not the palette has been solved, and in how many moves.
+
+### generate
+`./generate PALETTE_SIZE NUM_PUSHES OUTPUT_FILE`
+
+Takes in two integers and a file path. Generates a palette with PALETTE_SIZE rows and columns, performs NUM_PUSHES random pushes, and writes the result to OUTPUT_FILE
+
+### visualize
+`./visualize PALETTE_PATH PUSHES_PATH`
+
+Arguments are the same as `check`. Let's you watch all the moves happen in real time.
--- lib/board.py
+++ lib/board.py
@@ -1,3 +1,5 @@
+from moves import Move
+
 class Board:
     rows = []
     size = 0
@@ -9,13 +11,14 @@ class Board:
 
     def __init__(self, raw_matrix):
         self.size = len(raw_matrix)
-        for row in raw_matrix:
-            for col in self.size:
-                if row[col] == "*":
-                    self.hole_row = row
-                    self.hole_col = col
+        for row_index in range(self.size):
+            row = raw_matrix[row_index]
+            for col_index in range(self.size):
+                if row[col_index] == "*":
+                    self.hole_row = row_index
+                    self.hole_col = col_index
                 else:
-                    row[col] = int(row[col])
+                    row[col_index] = int(row[col_index])
 
             self.rows.append(row)
 
@@ -36,7 +39,20 @@ class Board:
         self.hole_col = move.col
 
     def can_move(self, move):
+        if max(move.row, move.col) >= self.size or min(move.row, move.col) < 0:
+            return False
+
         return abs(self.hole_row - move.row) + abs(self.hole_col - move.col) == 1
+
+    def valid_moves(self):
+        moves = [
+            Move([self.hole_row, self.hole_col + 1]),
+            Move([self.hole_row, self.hole_col - 1]),
+            Move([self.hole_row + 1, self.hole_col]),
+            Move([self.hole_row - 1, self.hole_col])
+        ]
+
+        return [move for move in moves if self.can_move(move)]
 
     def is_solved(self):
         last_seen = 0
@@ -51,12 +67,21 @@ class Board:
 
         return True
 
+    def write_to_file(self, filename):
+        f = open(filename, "w")
+
+        f.write(str(self.size) + "\n")
+        for row in self.rows:
+            f.write(' '.join(str(col) for col in row) + "\n")
+
+        f.close()
+
 def from_file(filename):
     f = open(filename, "r")
-    size = f.readline()
+    size = int(f.readline())
 
     rows = []
     for row in range(size):
-        rows.append(f.readline.split())
+        rows.append(f.readline().split())
 
     return Board(rows)
